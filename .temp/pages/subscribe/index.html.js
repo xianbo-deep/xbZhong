93edee67f9681b234e6dd2ca87a0832669839c0a
BO <2396768163@qq.com>
2026-02-13 00:50:12 +0800

    2026-02-13

--- .temp/pages/subscribe/index.html.js
+++ .temp/pages/subscribe/index.html.js
@@ -1,5 +1,5 @@
 import comp from "E:/Myown/Blog/.temp/pages/subscribe/index.html.vue"
-const data = JSON.parse("{\"path\":\"/subscribe/\",\"title\":\"订阅更新\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"订阅更新\",\"icon\":\"envelope\",\"sidebar\":false,\"article\":false},\"readingTime\":{\"minutes\":0.06,\"words\":19},\"filePathRelative\":\"subscribe/README.md\",\"excerpt\":\"<div style=\\\"margin-top: 50px;\\\"></div>\\n\"}")
+const data = JSON.parse("{\"path\":\"/subscribe/\",\"title\":\"订阅更新\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"订阅更新\",\"icon\":\"envelope\",\"sidebar\":false,\"article\":false,\"head\":[[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"订阅更新\\\"}\"],[\"meta\",{\"property\":\"og:url\",\"content\":\"https://xbzhong.cn/subscribe/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"阿b的博客\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"订阅更新\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"readingTime\":{\"minutes\":0.06,\"words\":19},\"filePathRelative\":\"subscribe/README.md\",\"excerpt\":\"<div style=\\\"margin-top: 50px;\\\"></div>\\n\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
